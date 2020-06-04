509c297767e8f51233816de19c4774ef5f1103a8
Vinicius <viniciusgil18@hotmail.com>
2020-06-04 17:43:33 -0300

    log

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://cadastrorifabackend.herokuapp.com/',
+    baseURL: 'https://cadastrorifabackend.herokuapp.com',
     //baseURL: 'http://localhost:3333',
 })
 
